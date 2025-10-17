1ee12703a9252c4243f07b5ddde663b1d360e98a
Calm1403 <harris.berry@outlook.com>
2025-10-17 21:27:38 +0100

    Edited circle.h.

--- circle.h
+++ circle.h
@@ -1,8 +1,21 @@
 #ifndef CIRCLE_H
 #define CIRCLE_H
 
-typedef struct board_s board_t; 
+#ifdef __linux__
+#ifndef __USE_MISC
+#define __USE_MISC
+#endif
+#endif
 
-void draw_circle(board_t* B, const int R, const int D, const int C, const int phi);
+#ifdef _WIN32
+#ifndef _USE_MATH_DEFINES
+#define _USE_MATH_DEFINES
+#endif
+#endif
+
+typedef struct board_s board_t;
+
+void
+draw_circle(board_t* B, const int R, const int D, const int C, const int phi);
 
 #endif
